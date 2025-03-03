713964df3c26fd7af7cd767ac6a0f1c9fa994738
Trowa-Pups <sainttrowa@gmail.com>
2025-03-03 13:54:12 +0800

    Delete OOP_Prog06.py

--- OOP_Prog06.py
+++
@@ -1,9 +0,0 @@
-def Prog6():
-    number1 = float(input("Input your first number: "))
-    number2 = float(input("Input your second number: "))
-
-    raised = number1 ** number2
-
-    print(raised)
-
-Prog6()
