3aff688e655359090e75dada73a914061d27474c
Trowa-Pups <sainttrowa@gmail.com>
2025-03-03 13:53:48 +0800

    Delete OOP_Prog03.py

--- OOP_Prog03.py
+++
@@ -1,9 +0,0 @@
-def Prog3():
-    number1 = float(input("Input your first number: "))
-    number2 = float(input("Input your second number: "))
-
-    sum = number1 + number2
-    
-    print(sum)
-
-Prog3()
